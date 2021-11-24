75104c9cbff99b841b297dd4e0822ecba55a472b
felipe19 <felipe55065@hotmail.com>
2021-11-23 21:23:33 -0500

    ajustes al api de proyectos

--- src/model/proyecto.js
+++ src/model/proyecto.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+const Autor = mongoose.model("Usuario");
 
-const ProyectoSchema = new Schema({
+const LoginSchema = new Schema({
   nombre_proyecto: { type: String, required: true },
-  presupuesto: { type: Number, required: true },
-  estado: { type: String, required: false },
-  fecha_inicio: { type: Date, required: false },
-  fecha_terminacion: { type: Date, },
-  face_proyecto: { type: String,required: false  },
   objetivo_general: { type: String, required: true },
-  objetivo_especifico: { type: Array, required: true },
-  id_usuario: [{ type: mongoose.Types.ObjectId, required: true, ref: "Usuario" }],
+  objetivo_especifico: { type: [], required: true },
+  presupuesto: { type: String, required: true },
+  fecha_inicio: { type: Date, required: true },
+  fecha_terminacion: { type: Date, required: true },
+  id_usuario: { type: Schema.ObjectId, ref: "Usuario" },
+  estado: {},
+  fase_proyecto: {},
 });
 module.exports = mongoose.model("Proyecto", ProyectoSchema, "proyecto");
