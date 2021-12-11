1a76908b1d236054bf31acc8132b336d6d7a6e51
gustavo <geramirez01@gmail.com>
2021-12-11 16:19:08 -0500

    act

--- src/Schema/Resolvers/avance.resolvers.js
+++ src/Schema/Resolvers/avance.resolvers.js
@@ -15,8 +15,8 @@ const mapearInput = async (input) => {
 module.exports.resolversAvance = {
   crearAvance: async ({ input }, context) => {
     
-    // const { usuarioVerificado } = context;
-    // if (!usuarioVerificado) throw new Error("Prohibido");
+    const { usuarioVerificado } = context;
+     if (!usuarioVerificado) throw new Error("Prohibido");
     const _avance = new Avance(await mapearInput({ ...input }));
     console.log(_avance);
     return await _avance.save();
