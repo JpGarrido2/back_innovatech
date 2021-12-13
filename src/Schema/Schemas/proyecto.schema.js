54bdbd4ab2f3081708d31c433b615a6ccbd1dc87
felipe19 <felipe55065@hotmail.com>
2021-12-13 16:39:08 -0500

    ajuster al resolver de proyectos

--- src/Schema/Schemas/proyecto.schema.js
+++ src/Schema/Schemas/proyecto.schema.js
@@ -2,13 +2,13 @@ module.exports.DEFINICION_PROYECTOS = `
     type Proyecto {
         _id: ID!
         objetivo_general: String!
-        objetivo_especifico: [Objetivo_especifico!]!
+        objetivo_especifico: String!
         nombre_proyecto: String!
         presupuesto: Int!
         estado: String!
         fecha_inicio: Date!
         fecha_terminacion: Date
-        face_proyecto: String
+        fase_proyecto: String
         id_usuario: [UsuarioProyecto]
         
     
@@ -16,7 +16,7 @@ module.exports.DEFINICION_PROYECTOS = `
 
     type Objetivo_especifico{
         objetivo_especifico:String!
-        fecha_creacion: Date!
+       
     }
     type UsuarioProyecto{
         _id: ID!
@@ -46,9 +46,9 @@ module.exports.DEFINICION_PROYECTOS = `
         estado: String
         fecha_inicio: Date
         fecha_terminacion: Date
-        face_proyecto: String
+        fase_proyecto: String
         objetivo_general: String
-        objetivo_especifico: [ObjetivoInput!]!
+        objetivo_especifico: String
         id_usuario: String
     }
 
@@ -61,7 +61,7 @@ module.exports.DEFINICION_PROYECTOS = `
         estado: String
         fecha_inicio: Date
         fecha_terminacion: Date
-        face_proyecto: String
+        fase_proyecto: String
         objetivo_general: String
         objetivo_especifico: String
         id_usuario: String
@@ -70,7 +70,7 @@ module.exports.DEFINICION_PROYECTOS = `
     input UpdateProyecto{
         nombre_proyecto: String
         objetivo_general: String
-        objetivo_especifico: [ObjetivoInput]
+        objetivo_especifico: String
         presupuesto: Int
     }
 `;
@@ -84,10 +84,11 @@ module.exports.QUERY_PROYECTOS = `
     proyecto_estado(estado: String!): [Proyecto!]!
     proyecto_fecha_inicio(fecha_inicio: Date!): Proyecto
     proyecto_fecha_terminacion(fecha_terminacion: Date!): Proyecto
-    proyecto_face_proyecto(face_proyecto: String!): Proyecto
+    proyecto_fase_proyecto(fase_proyecto: String!): Proyecto
     proyecto_objetivo_general(objetivo_general: String!): Proyecto
     proyecto_objetivo_especifico(objetivo_especifico: String!): Proyecto
     proyecto_id_usuario(id_usuario: String!): [Proyecto]
+    
 `;
 
 module.exports.MUTATIONS_PROYECTOS = `
@@ -96,7 +97,7 @@ module.exports.MUTATIONS_PROYECTOS = `
     updateProyecto(_id: ID!, input: UpdateProyecto): Proyecto
     actualizar_fecha_terminacion(fecha_terminacion: String!, input:ProyectoAInput):Proyecto
     actualizarproyecto_estado(_id: ID!,input: ProyectoAInput): Proyecto
-    actualizarface_proyecto(_id: ID!, input: ProyectoAInput):Proyecto
+    actualizarfase_proyecto(_id: ID!, input: ProyectoAInput):Proyecto
     eliminarproyecto_objetivo_general(objetivo_general:String!): Proyecto
     eliminarproyecto_ID(_id: ID!): Proyecto
 `;
